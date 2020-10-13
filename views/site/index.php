c302b7c8424c83f05fba05b25df96c209109ce0c
Daniel <palm6991@gmail.com>
2020-10-13 13:38:20 +0300

    aa

--- views/site/index.php
+++ views/site/index.php
@@ -19,3 +19,5 @@ $bookshelf = json_decode($data, TRUE);
 echo "<pre>";
 print_r($bookshelf);
 echo "</pre>";
+
+//https://api.telegram.org/bot1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk/setWebhook?url=https://demo-telegram-stage.herokuapp.com
