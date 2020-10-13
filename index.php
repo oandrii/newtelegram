b9739475a54dab335113b460d2151453a772d23d
Daniel <palm6991@gmail.com>
2020-10-13 13:22:02 +0300

    aa

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
 $filename = 'log.log';
 
-//$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
-//$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
-//$result = file_get_contents($url);
+$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
+$result = file_get_contents($url);
 
-$result = $_POST;
+//$result = $_POST;
 $result = json_encode($result, true);
 file_put_contents($filename, $result);
 
 
-$data = file_get_contents($filename);
-$bookshelf = json_decode($data, TRUE);
-
-echo "<pre>";
-print_r($bookshelf);
-echo "</pre>";
--- message.php
+++ message.php
@@ -1,1 +1,10 @@
-qqwwee
+<?php
+
+$filename = 'log.log';
+
+$data = file_get_contents($filename);
+$bookshelf = json_decode($data, TRUE);
+
+echo "<pre>";
+print_r($bookshelf);
+echo "</pre>";
