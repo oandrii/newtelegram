9a940b4309c5d2de76b9ab37d9d368df6919b4ca
Daniel <palm6991@gmail.com>
2020-10-13 12:02:56 +0300

    aa

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-helloqwe
+<?php
+
+$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+
+$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
+$result = file_get_contents($url);
+$result = json_decode($result, true);
+
+var_dump($result);
