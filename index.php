4a58d223f6a0c606588747f531392861dcb6ea15
Daniel <palm6991@gmail.com>
2020-10-13 15:43:35 +0300

    aa

--- index.php
+++ index.php
@@ -9,5 +9,21 @@ $data = file_get_contents('php://input');
 file_put_contents($filename, $data);
 
 
+$message = ($data['message']['text']);
+if ($message === "123") {
+    $inline_button1 = array("text" => "111");
+    $inline_button2 = array("text" => "222");
+    $inline_keyboard = [[$inline_button1, $inline_button2]];
+    $keyboard = array("inline_keyboard" => $inline_keyboard);
+    $replyMarkup = json_encode($keyboard);
+}
+
+function sendMessage($chat_id, $message, $replyMarkup)
+{
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
+}
+
+$chat_id = 718524282;
+sendMessage($chat_id, "ok", $replyMarkup);
 
 
