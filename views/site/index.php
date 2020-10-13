5c0f2bb1f7ba58c14b4baf5c323f7919183eac7b
Daniel <palm6991@gmail.com>
2020-10-13 12:31:47 +0300

    aa

--- views/site/index.php
+++ views/site/index.php
@@ -1,9 +1,13 @@
 <?php
-$filename = 'array.txt';
-$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+$filename = '/web/array.txt';
+//$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+//
+//$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
+//$result = file_get_contents($url);
 
-$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
-$result = file_get_contents($url);
+$result = $_POST;
+print_r($result);
+die;
 
 
 file_put_contents($filename, $result);
