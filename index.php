9c480bec2ad43c3a9591629711d29aecd99d087a
Daniel <palm6991@gmail.com>
2020-10-13 12:36:22 +0300

    aa

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $filename = 'log.log';
-$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
 
-$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
-$result = file_get_contents($url);
+//$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+//$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
+//$result = file_get_contents($url);
+
+$result = "123";
 
 //$result = $_POST;
 //print_r($result);
