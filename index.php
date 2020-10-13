1efebae9eae3eaf09f7730a9d0573f290b4e9016
Daniel <palm6991@gmail.com>
2020-10-13 12:24:48 +0300

    aa

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-$filename = 'array.txt';
+$filename = '/web/array.txt';
 $bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
 
 $url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
 $result = file_get_contents($url);
-$result = json_decode($result);
+$result = json_decode($result, true);
 
 file_put_contents($filename, $result);
 
