b046b61fd03f5c7df87f604c2b27b8a370ca4690
Daniel <palm6991@gmail.com>
2020-10-13 12:35:09 +0300

    aa

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-$filename = '/web/array.txt';
+$filename = 'log.log';
 $bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
 
 $url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
 $result = file_get_contents($url);
-$result = json_decode($result, true);
+
+//$result = $_POST;
+//print_r($result);
+//die;
+//
 
 file_put_contents($filename, $result);
 
--- views/site/index.php
+++ views/site/index.php
@@ -1,14 +1,14 @@
 <?php
-$filename = '/web/array.txt';
-//$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+$filename = 'log.log';
+$bot_id = "1372199341:AAEG7UXyMvVYpHukmbnAwvwh4VU7rxH1gQk";
+
+$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
+$result = file_get_contents($url);
+
+//$result = $_POST;
+//print_r($result);
+//die;
 //
-//$url = 'https://api.telegram.org/bot' . $bot_id . '/getUpdates?offset=0';
-//$result = file_get_contents($url);
-
-$result = $_POST;
-print_r($result);
-die;
-
 
 file_put_contents($filename, $result);
 
