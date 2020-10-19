8ba33af89b29673f96b24c5c1d73c54f7b51890a
Daniel <palm6991@gmail.com>
2020-10-19 09:54:18 +0300

    aa

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
 $filename = 'log.log';
 $data = file_get_contents('php://input');
 file_put_contents($filename, $data);
