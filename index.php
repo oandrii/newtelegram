f23026ca0daaf83265668cf38162feabed2d40c3
Daniel <palm6991@gmail.com>
2020-10-26 12:30:00 +0200

    aa

--- index.php
+++ index.php
@@ -19,36 +19,33 @@ $client = new Client( array( 'base_uri' => $apiURL ) );
 $update = json_decode( file_get_contents( 'php://input' ) );
 
 if ( $update->message->text == '/start' ) {
-    $keyboard = array(
-        "keyboard" => array(array(
-            array(
-                "text" => "contact",
+    $keyboard = [
+        "keyboard" => [[
+            [
+                "text" => "Send my contact details",
                 "request_contact" => true // This is OPTIONAL telegram button
-            ),
-
-        )),
-        "one_time_keyboard" => false, // Can be FALSE (hide keyboard after click)
-        "resize_keyboard" => false // Can be FALSE (vertical resize)
-    );
+            ],
+        ]],
+        "one_time_keyboard" => true, // Can be FALSE (hide keyboard after click)
+        "resize_keyboard" => true // Can be FALSE (vertical resize)
+    ];
     $client->post('sendMessage', array('query' => array(
         'chat_id' => $update->message->chat->id,
         'text' => "Share you contacts",
         'reply_markup' => json_encode($keyboard))));
 }
 else {
-    $keyboard = array(
-        "keyboard" => array(array(
-            array(
+    $keyboard = [
+        "keyboard" => [[
+            [
                 "text" => "text",
-            ),
-            array(
+            ],[
                 "text2" => "text2",
-            ),
-
-        )),
-        "one_time_keyboard" => false, // Can be FALSE (hide keyboard after click)
-        "resize_keyboard" => false // Can be FALSE (vertical resize)
-    );
+            ],
+        ]],
+        "one_time_keyboard" => true, // Can be FALSE (hide keyboard after click)
+        "resize_keyboard" => true // Can be FALSE (vertical resize)
+    ];
     $client->post('sendMessage', array('query' => array(
         'chat_id' => $update->message->chat->id,
         'text' => ":)",
